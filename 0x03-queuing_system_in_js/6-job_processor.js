c8badf7d6a618d840207a8ef1941074bc60c2346
Tom Morara <morara1920@gmail.com>
2025-06-18 18:26:21 +0300

    implement job processor to handle push_notification_code queue with sendNotification logic

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,13 @@
+// Notification sender
+
+const kue = require('kue');
+const queue = kue.createQueue();
+
+queue.process('push_notification_code', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message);
+  done();
+});
+
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
